21114b03e4bab255ad9ff6721741e1d0311b890f
andreaservin <andiserm@gmail.com>
2023-06-27 15:56:14 -0400

    Cleaned app.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Home from './screens/Home';
 
 export default function App() {
@@ -6,12 +6,3 @@ export default function App() {
     <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
