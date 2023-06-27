993d133a6d2a8689dd785dfc184bbc171086a6e7
andreaservin <andiserm@gmail.com>
2023-06-27 15:54:15 -0400

    Removed unused code

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Options from './screens/Options';
 import Home from './screens/Home';
 
 export default function App() {
